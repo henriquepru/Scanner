8e41d1b52abd550642fa105e89e60ccd35eca254
Henrique Prudencio <henrique.santos@concrete.com.br>
2018-03-10 21:53:38 -0300

    Initial Commit

---
+++ Scanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scanner
+//
+//  Created by Henrique Santos on 10/03/18.
+//  Copyright © 2018 Henrique Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
