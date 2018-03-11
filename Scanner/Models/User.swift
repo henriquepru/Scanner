25ed338bf4613a9ab8b4b9147c3c37da83ad4f78
Henrique Prudencio <henrique.santos@concrete.com.br>
2018-03-10 22:58:42 -0300

    Added User Model and LoginViewController

---
+++ Scanner/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Scanner
+//
+//  Created by Henrique Santos on 10/03/18.
+//  Copyright © 2018 Henrique Santos. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    let id: Int
+    let userName: String
+    let password: String
+}
---
+++ Scanner/Scenes/Login/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  Scanner
+//
+//  Created by Henrique Santos on 10/03/18.
+//  Copyright © 2018 Henrique Santos. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
